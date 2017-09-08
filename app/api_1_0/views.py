f7673a240f170b2a714e351ee42259095c096533
liuqi <liuqi0315@gmail.com>
2017-09-08 17:53:08 +0800

    feature: complete GET API

--- app/api_1_0/views.py
+++ app/api_1_0/views.py
@@ -1,6 +1,17 @@
 from . import api
+from flask import jsonify, abort
+from app.models import Tasks
 
 
-@api.route('/', methods=['GET'])
+@api.route('/tasks', methods=['GET'])
 def index():
-    return 'Hello World!'
+    tasks = Tasks.query.all()
+    return jsonify({'tasks': list(map(lambda task: task.get_json(), tasks))})
+
+
+@api.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = Tasks.query.filter_by(id=task_id).first()
+    if not task:
+        abort(404)
+    return jsonify({'task': task.get_json()})
